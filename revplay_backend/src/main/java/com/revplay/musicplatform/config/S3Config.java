1cd083ca60a461e5fece9d0603681d4f918aaeb6
kalyan <kalyankambhampati1729@gmail.com>
2026-03-23 00:33:28 +0530

    Repackage Git index: Remove tracked build artifacts and apply .gitignore

--- revplay_backend/src/main/java/com/revplay/musicplatform/config/S3Config.java
+++ revplay_backend/src/main/java/com/revplay/musicplatform/config/S3Config.java
@@ -3,7 +3,9 @@ package com.revplay.musicplatform.config;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -14,14 +16,15 @@ public class S3Config {
 
     @Bean
     public S3Client s3Client(AwsProperties awsProperties) {
-        AwsBasicCredentials credentials = AwsBasicCredentials.create(
-            awsProperties.getAccessKey(),
-            awsProperties.getSecretKey()
-        );
-
-        return S3Client.builder()
-            .region(Region.of(awsProperties.getRegion()))
-            .credentialsProvider(StaticCredentialsProvider.create(credentials))
-            .build();
+        var builder = S3Client.builder().region(Region.of(awsProperties.getRegion()));
+        if (StringUtils.hasText(awsProperties.getAccessKey())
+                && StringUtils.hasText(awsProperties.getSecretKey())) {
+            builder.credentialsProvider(StaticCredentialsProvider.create(
+                    AwsBasicCredentials.create(awsProperties.getAccessKey(), awsProperties.getSecretKey())));
+        } else {
+            // EC2 instance profile / IAM role, env vars, etc. (avoids "Access key ID cannot be blank")
+            builder.credentialsProvider(DefaultCredentialsProvider.create());
+        }
+        return builder.build();
     }
 }
